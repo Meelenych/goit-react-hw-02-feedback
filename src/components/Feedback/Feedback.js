3659341da880a986bfd8cec65ec7185ea51b83c2
Meelenych <meelenych@gmail.com>
2021-11-23 00:01:10 +0200

    formatting added

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import s from './Feedback.module.css';
+import React from "react";
+import PropTypes from "prop-types";
+import s from "./Feedback.module.css";
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-    return (
-        <div className={s.form}>
-            {options.map(option => (
-                <button
-                    className={s.button}
-                    type="button"
-                    key={option}
-                    onClick={() => onLeaveFeedback(option)}
-                >
-                {option}
-                </button>
-            ))}
-        </div>
-    );
+	return (
+		<div className={s.form}>
+			{options.map((option) => (
+				<button
+					className={s.button}
+					type="button"
+					key={option}
+					onClick={() => onLeaveFeedback(option)}
+				>
+					{option}
+				</button>
+			))}
+		</div>
+	);
 };
 
 FeedbackOptions.propTypes = {
-  onLeaveFeedback: PropTypes.func.isRequired,
-  options: PropTypes.array.isRequired,
+	onLeaveFeedback: PropTypes.func.isRequired,
+	options: PropTypes.array.isRequired,
 };
 
 export default FeedbackOptions;
--- src/components/Feedback/Section.js
+++ src/components/Feedback/Section.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './Section.module.css';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./Section.module.css";
 
 const Section = ({ title, children }) => {
-    return (
-        <section className={styles.section}>
-            <h2>{title}</h2>
-            {children}
-        </section>
-    );
+	return (
+		<section className={styles.section}>
+			<h2>{title}</h2>
+			{children}
+		</section>
+	);
 };
 
 Section.propTypes = {
-  title: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
 };
 
 export default Section;
